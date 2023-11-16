408bfebe093b350491e15db456b096e7a7a117d3
Justice Markwei <justice@hubtel.com>
2023-11-16 20:30:15 +0000

    Configure env file

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import Home from "./pages/home";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Home></Home>
-    </>
-  );
+	return (
+		<>
+			<Home></Home>
+		</>
+	);
 }
 
 export default App;
